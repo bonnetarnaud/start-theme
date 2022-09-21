a900a940a137d6cf3ad1f4b9f9db32e7fb2a3ab2
arnaud bonnet <2a.bonnet@gmail.com>
2022-09-21 11:06:08 +0200

    add title

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
                 <div class="row">
                     <div class="col-12">
                     <?php while (have_posts()) : the_post(); ?>
+                    <?php the_title(); ?>
                         <?php the_content() ?>
                     <?php endwhile; ?>
                     </div>
